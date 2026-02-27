efc5b3cf30631819197819a9764373599e72af66
Bernd Donner <bernd.donner@sabel.com>
2026-02-27 16:35:50 +0100

    virtualbox raus

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Tabby-Replacement (MCT) - NixOS 25.11 VMware image config";
+  description = "Microcontrollertechnik - NixOS 25.11 VMware image config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
@@ -10,24 +10,15 @@
   outputs = { self, nixpkgs, nixos-generators }:
     let
       system = "x86_64-linux";
+      mk = format: nixos-generators.nixosGenerate {
+        inherit system format;
+        modules = [ ./modules/mct-vm.nix ];
+      };
     in {
-      packages.${system}.vmware = nixos-generators.nixosGenerate {
-        inherit system;
-        format = "vmware";
-        modules = [ ./modules/mct-vm.nix ];
-      };
-
-      # Optional formats
-      packages.${system}.qcow2 = nixos-generators.nixosGenerate {
-        inherit system;
-        format = "qcow2";
-        modules = [ ./modules/mct-vm.nix ];
-      };
-
-      packages.${system}.virtualbox = nixos-generators.nixosGenerate {
-        inherit system;
-        format = "virtualbox";
-        modules = [ ./modules/mct-vm.nix ];
+      packages.${system} = {
+        vmware = mk "vmware";
+        qcow2  = mk "qcow2";   # optional, kann auch komplett raus
+        default = mk "vmware"; # optional: nix build ohne .#...
       };
     };
 }
