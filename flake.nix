e794d7ecca7de5b199791b0e563c834236ff41c4
Bernd Donner <bernd.donner@sabel.com>
2026-03-01 19:53:58 +0100

    missing flake.nix added

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Microcontrollertechnik - NixOS 25.11 VMware image config";
+  description = "Microcontrollertechnik - NixOS 25.11 VM image config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
@@ -11,23 +11,50 @@
   outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
+      lib = nixpkgs.lib;
 
-      bunnySystem = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          home-manager.nixosModules.home-manager
-          ./modules/mct-vm.nix
-        ];
-      };
+      username = "student";
+
+      # Create host list: bunny + bunny00..bunny11
+      ids =
+        [ "bunny" ]
+        ++ (map (n: "bunny" + (lib.fixedWidthNumber 2 n)) (lib.range 0 11));
+
+      hostFileFor = host:
+        let p = ./hosts + ("/" + host + ".nix");
+        in if builtins.pathExists p then p else ./hosts/default.nix;
+
+      mkHost = host:
+        let h = import (hostFileFor host);
+        in nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            home-manager.nixosModules.home-manager
+            ./modules/mct-vm.nix
+
+            # Host-specific settings (Nix-managed, reproducible)
+            ({ ... }: {
+              networking.hostName = host;
+
+              home-manager.users.${username}.programs.git = {
+                userName  = h.gitName;
+                userEmail = h.gitEmail;
+              };
+            })
+          ];
+        };
+
+      nixosConfs =
+        builtins.listToAttrs (map (host: { name = host; value = mkHost host; }) ids);
+
+      bunnySystem = nixosConfs.bunny;
     in {
-      nixosConfigurations.bunny = bunnySystem;
+      nixosConfigurations = nixosConfs;
 
       packages.${system} = {
-        # neue upstreamed image builder: system.build.images.<variant>
-        # Variantenamen sind wie bei nixos-generators (z.B. vmware, qcow-efi, raw-efi, ...)
-        vmware = bunnySystem.config.system.build.images.vmware;
+        # nixpkgs images (upstreamed nixos-generators functionality)
         qcow2  = bunnySystem.config.system.build.images."qemu-efi";
-
+        vmware = bunnySystem.config.system.build.images.vmware;
         default = bunnySystem.config.system.build.images.vmware;
       };
     };
